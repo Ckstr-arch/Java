837e9908af8ae028437156bb332b9988a5bac48f
Dark-str <sasithisurutc@gmail.com>
2026-09-09 11:56:57 +0530

    Java Initialization

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+/*
+ echo "# Academia" >> README.md
+ git init
+ git add README.md
+ git add *
+ git commit -m "Java Initialization"
+ git branch -M main
+ git remote add origin https://github.com/Ckstr-arch/Academia.git
+ git push -u origin main */
+ 
---
+++ src/universe.java
@@ -0,0 +1,6 @@
+public class universe {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Hello, Universe!");
+    }
+}
